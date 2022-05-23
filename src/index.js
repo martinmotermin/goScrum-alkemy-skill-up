f38b258ee670ddc2bf166c7482eb9e440bf1d79a
Martin Otermin <martinm.otermin@gmail.com>
2022-05-23 11:34:03 -0300

    feat: basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 
 render(
-  <BrowserRouter>
+  <BrowserRouter basename="goScrum-alkemy-skill-up">
     <Provider store={store}>
       <App />
     </Provider>
